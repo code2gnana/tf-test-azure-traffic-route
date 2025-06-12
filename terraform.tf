40a35ec162d51cea180fc1a677622b0b9295b40d
Gnaneshwara Babu <mail2gnana@gmail.com>
2025-06-12 23:52:41 +1000

    Add Azure provider configuration and variable definitions

---
+++ terraform.tf
@@ -0,0 +1,9 @@
+terraform {
+    required_providers {
+         azurerm = {
+              source  = "hashicorp/azurerm"
+              version = "~>3.91.0"
+         }
+    }
+    required_version = ">= 0.5.0"
+}
--- variables.tf
+++ variables.tf
@@ -3,3 +3,16 @@ variable "location" {
   default = "australiaeast"
   description = "The Azure region where resources will be created."
 }
+
+variable "subscription_id" {
+  description = "The Azure subscription ID where resources will be created."
+}
+variable "client_id" {
+  description = "The Azure client ID for authentication."
+}
+variable "client_secret" {
+  description = "The Azure client secret for authentication."
+}
+variable "tenant_id" {
+  description = "The Azure tenant ID for authentication."
+}
